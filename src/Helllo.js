af99d0be86c9a95895a43a76d5c71fc65674340f
ahsanajaz786 <ahsanajaz2@gmail.com>
2020-06-06 23:20:56 +0500

    basics react1

---
+++ src/Helllo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const HELLO=() =>
+   (
+  <h1>HELLO   </h1>
+  );
+
+
+export default HELLO;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Helllo'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <div>
    <App name="Ahsan Ajaz" />
+   <Hello></Hello>
 
   </div>,
   document.getElementById('root')
