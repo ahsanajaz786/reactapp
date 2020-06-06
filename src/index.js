1e41917093c352e973b3823847deb651166160a6
ahsanajaz786 <ahsanajaz2@gmail.com>
2020-06-06 23:32:24 +0500

    basics react11q

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Hello from './Helllo'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
